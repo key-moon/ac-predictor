77edb3046c471e042735334e90ed12d2b02ffbf7
key-moon <32513370+key-moon@users.noreply.github.com>
2018-06-06 22:29:07 +0900

    [add] APrefを取得するAPIのClassを作成

---
+++ ac-predictor/API/APrefsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ac_predictor.API
+{
+    public class APrefsController : ApiController
+    {        
+        public string Get(string id)
+        {
+            return "value";
+        }
+    }
+}
