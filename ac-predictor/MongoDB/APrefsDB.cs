a5884dfdf3081d6d95d87fec168e6936e496b30f
key-moon <32513370+key-moon@users.noreply.github.com>
2018-06-08 01:01:34 +0900

    [add] インスタンスの初期化の追加

--- ac-predictor/MongoDB/APrefsDB.cs
+++ ac-predictor/MongoDB/APrefsDB.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Configuration;
 using MongoDB;
 using MongoDB.Driver;
 using MongoDB.Driver.Core;
@@ -11,5 +12,18 @@ namespace ac_predictor.MongoDB
 {
     public class APrefsDB
     {
+        private string connectionString => ConfigurationManager.AppSettings["MongoDBConnection"];
+        private static string DBName = "Predictor";
+        private static string TableName = "APrefs";
+
+        private MongoClient client;
+        private IMongoDatabase database;
+        private IMongoCollection<Dictionary<string, double>> collection;
+        public APrefsDB()
+        {
+            client = new MongoClient(connectionString);
+            database = client.GetDatabase(DBName);
+            collection = database.GetCollection<Dictionary<string, double>>(TableName);
+        }
     }
 }
