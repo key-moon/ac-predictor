a9e3cdbf6b9aed89f8b763544a071b77436a9cea
key-moon <32513370+key-moon@users.noreply.github.com>
2018-06-07 18:42:25 +0900

    [add] MongoDBのNuggetパッケージとUsingのを追加

--- ac-predictor/MongoDB/APrefsDB.cs
+++ ac-predictor/MongoDB/APrefsDB.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MongoDB;
+using MongoDB.Driver;
+using MongoDB.Driver.Core;
+using MongoDB.Driver.Linq;
 
 namespace ac_predictor.MongoDB
 {
