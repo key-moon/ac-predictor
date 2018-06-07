3b21099a506e5c6ff9d51f2b18e614b7f54cdad5
key-moon <32513370+key-moon@users.noreply.github.com>
2018-06-08 01:09:51 +0900

    [add] APrefsクラスを追加

--- ac-predictor/MongoDB/APrefsDB.cs
+++ ac-predictor/MongoDB/APrefsDB.cs
@@ -8,6 +8,9 @@ using MongoDB.Driver;
 using MongoDB.Driver.Core;
 using MongoDB.Driver.Linq;
 
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace ac_predictor.MongoDB
 {
     public class APrefsDB
@@ -24,6 +27,20 @@ namespace ac_predictor.MongoDB
             client = new MongoClient(connectionString);
             database = client.GetDatabase(DBName);
             collection = database.GetCollection<Dictionary<string, double>>(TableName);
+        }
+    }
+
+    public class APrefs
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        private string _id;
+        private Dictionary<string, double> aPrefs;
+
+        [BsonConstructor]
+        public APrefs(string _id, Dictionary<string, double> APrefs)
+        {
+            this._id = _id;
+            aPrefs = APrefs;
         }
     }
 }
