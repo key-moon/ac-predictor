205b48d92b368968bf9c2f6430d8d8833c5e3ab7
key-moon <32513370+key-moon@users.noreply.github.com>
2018-06-08 19:27:56 +0900

    [add] データ更新の追加

--- ac-predictor/AtCoder/Standing.cs
+++ ac-predictor/AtCoder/Standing.cs
@@ -24,7 +24,7 @@ namespace ac_predictor.AtCoder
             StandingsData = standingsData;
         }
 
-        static Standings GetStandings(string contestID) => Scraping.GetStandings(contestID);
+        public static Standings GetStandings(string contestID) => Scraping.GetStandings(contestID);
     }
     public class TaskInfo
     {
