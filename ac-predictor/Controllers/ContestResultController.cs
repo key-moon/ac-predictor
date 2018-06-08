6ca6b5e4a03b43b63e0754abb163ef0c7fdbdf44
key-moon <32513370+key-moon@users.noreply.github.com>
2018-06-09 02:16:01 +0900

    [add] 最低限のファイルを追加

---
+++ ac-predictor/Controllers/ContestResultController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ac_predictor.Controllers
+{
+    public class ContestResultController : Controller
+    {
+        // GET: ContestResult
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("{controller}/{contestID}")]
+        public ActionResult Result(string contestID)
+        {
+            return View();
+        }
+    }
+}
