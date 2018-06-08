73d52f93d2e304e59e9b1ca467cb29ab7d12d62e
key-moon <32513370+key-moon@users.noreply.github.com>
2018-06-08 12:53:01 +0900

    [add] コンテスト一覧を取る

--- ac-predictor/API/APrefsController.cs
+++ ac-predictor/API/APrefsController.cs
@@ -4,11 +4,21 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Newtonsoft.Json;
+using ac_predictor.MongoDB;
 
 namespace ac_predictor.API
 {
     public class APrefsController : ApiController
-    {        
+    {
+        /// <summary>DBに入ってるコンテスト一覧の取得</summary>
+        public string Get()
+        {
+            APrefsDB db = new APrefsDB();
+            string[] contestIDs = db.ContestIDs.ToArray();
+            return JsonConvert.SerializeObject(contestIDs);
+        }
+
         public string Get(string id)
         {
             return "value";
