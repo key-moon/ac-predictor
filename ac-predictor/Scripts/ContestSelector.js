fb9f009df5e1667f3ad54307e91161838f66c3ef
key-moon <32513370+key-moon@users.noreply.github.com>
2018-06-10 03:49:12 +0900

    [add] コンテスト一覧を読み込む

---
+++ ac-predictor/Scripts/ContestSelector.js
@@ -0,0 +1,16 @@
+﻿var selector = document.getElementById("contestselector")
+
+var ContestJson = 
+$.ajax({
+	type: 'GET',
+	dataType: 'json',
+	url: 'http://ac-predictor.azurewebsites.net/api/aperfs',
+	async: false
+	}).responseText
+
+JSON.parse(ContestJson)
+	.forEach(
+	function (element) {
+		selector.appendChild(`<option>${element}</option>`);
+	}
+)
