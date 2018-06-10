aafdcd263b72e0159c34c02295c8645f10c79918
key-moon <32513370+key-moon@users.noreply.github.com>
2018-06-10 22:17:28 +0900

    [add] Contest結果Jsonを返す(AtCoderのを弾くだけ)

---
+++ ac-predictor/API/StandingsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Configuration;
+using System.Web.Http;
+using System.Web.Http.Results;
+using Newtonsoft.Json;
+using ac_predictor.MongoDB;
+using ac_predictor.AtCoder;
+
+namespace ac_predictor.API
+{
+    public class StandingsController : ApiController
+    {
+        [Route("standings/{contestID}")]
+        public JsonResult<Standings> Get(string contestID)
+        {
+            Standings result = Standings.GetStandings(contestID);
+            return Json(result);
+        }
+    }
+}
