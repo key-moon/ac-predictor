a7247a75dd0909182d61574dddb44741c67dede3
key-moon <32513370+key-moon@users.noreply.github.com>
2018-06-16 12:17:23 +0900

    [add] ユーザースクリプトファイルを追加

---
+++ ac-predictor.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        ac-predictor
+// @namespace   http://ac-predictor.azurewebsites.net/
+// @version     0.1.0
+// @description コンテスト中にAtCoderのパフォーマンスを予測します
+// @includes    https://beta.atcoder.jp/contests/*
+// @excludes    
+// ==/UserScript==
