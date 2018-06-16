1cb9d124fe6d24d9a685194baa51227bb47f1081
key-moon <32513370+key-moon@users.noreply.github.com>
2018-06-16 11:56:37 +0900

    [add] 全てのページに読み込まれるスタイルとJSを追加

---
+++ ac-predictor/Scripts/base.js
@@ -0,0 +1,8 @@
+﻿const colors = ["unrated", "gray", "brown", "green", "cyan", "blue", "yellow", "orange", "red"]
+function getColor(rating) {
+	var colorIndex = 0
+	if (rating > 0) {
+		colorIndex = Math.min(Math.floor(rating / 400) + 1, 8)
+	}
+	return colors[colorIndex]
+}
