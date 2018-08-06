5502ccce65dbb4ed87cb5a5fdac2a71cff00160b
key-moon <32513370+key-moon@users.noreply.github.com>
2018-08-06 16:03:05 +0900

    [change] Headerの修正(サポートURLの変更やhomepageの削除)

--- ac-predictor/predictor/header.js
+++ ac-predictor/predictor/header.js
@@ -5,7 +5,6 @@
 // @description コンテスト中にAtCoderのパフォーマンスを予測します
 // @author      keymoon
 // @license     MIT
-// @homepage    https://github.com/key-moon/ac-predictor
-// @supportURL  https://github.com/key-moon/ac-predictor/issues
+// @supportURL  http://ac-predictor.azurewebsites.net/support
 // @match       https://beta.atcoder.jp/*
 // ==/UserScript==
