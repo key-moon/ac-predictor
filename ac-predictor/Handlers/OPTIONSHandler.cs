03a5096e55662397b19270771ae5154295012cd4
key-moon <32513370+key-moon@users.noreply.github.com>
2018-08-13 23:20:39 +0900

    [add] オプションのハンドラを追加

---
+++ ac-predictor/Handlers/OPTIONSHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ac_predictor.Util;
+
+namespace ac_predictor.Handlers
+{
+    public class OPTIONSHandler : IHttpHandler
+    {
+        public bool IsReusable => true;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpRequest request = context.Request;
+            HttpResponse responce = context.Response;
+            if (!request.Headers.AllKeys.Contains("Origin")) return;
+            var requestUrl = request.Url;
+            var originUrl = new Uri(request.Headers["Origin"]);
+            if (!CrossOriginSettings.IsAllowed(requestUrl, originUrl)) return;
+            responce.Headers.Add("Access-Control-Allow-Origin", originUrl.GetLeftPart(UriPartial.Authority));
+            responce.Headers.Add("Access-Control-Allow-Methods", "GET");
+            responce.Headers.Add("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Origin");
+            responce.Headers.Add("Access-Control-Max-Age", "86400");
+        }
+    }
+}
