f0e7d3ca004645f67a3427286ecb65c08b4afb82
key-moon <32513370+key-moon@users.noreply.github.com>
2018-08-20 05:16:31 +0900

    [fix] object.keysの仕様の把握ミス

--- ac-predictor/predictor/ac-predictor.user.js
+++ ac-predictor/predictor/ac-predictor.user.js
@@ -696,9 +696,7 @@ SideMenu.Elements.Predictor = (async () => {
 	        \$('#predictor-reload').button('loading');
 	        AddAlert('順位表読み込み中…');
 	        SideMenu.Datas.Update.APerfs().then(SideMenu.Datas.Update.Standings).then(() => {
-	            console.log(SideMenu.Datas.APerfs);
-	            console.log(SideMenu.Datas.APerfs.length);
-	            if (SideMenu.Datas.APerfs.length === 0) {
+	            if (SideMenu.Datas.APerfs.keys().length === 0) {
 	                disabled();
 	                AddAlert('APerfのデータが提供されていません');
 	                return;
