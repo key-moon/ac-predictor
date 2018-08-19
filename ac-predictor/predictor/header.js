7702fd40bf733e85829b427dad2b3fc071f611df
key-moon <32513370+key-moon@users.noreply.github.com>
2018-08-20 04:46:42 +0900

    [change] version, supportURL

--- ac-predictor/predictor/header.js
+++ ac-predictor/predictor/header.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name        ac-predictor
 // @namespace   http://ac-predictor.azurewebsites.net/
-// @version     0.1.4
+// @version     1.0.0
 // @description コンテスト中にAtCoderのパフォーマンスを予測します
 // @author      keymoon
 // @license     MIT
-// @supportURL  http://ac-predictor.azurewebsites.net/support
+// @supportURL  https://github.com/key-moon/ac-predictor.user.js
 // @match       https://beta.atcoder.jp/*
 // @exclude     https://beta.atcoder.jp/*/json
 // ==/UserScript==
