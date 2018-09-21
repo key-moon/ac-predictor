1061f815eabb2e6e4aed522230cc1e7133a7afeb
key-moon <32513370+key-moon@users.noreply.github.com>
2018-09-21 19:56:51 +0900

    [change] ユーザー名検索のスクリプト位置を変更

--- ac-predictor/Scripts/ContestSelector.js
+++ ac-predictor/Scripts/ContestSelector.js
@@ -19,6 +19,41 @@
         localStorage.setItem('predictor-contests', JSON.stringify(contests));
     });
 
+    //ユーザー名検索
+    $('#username-search-button').click(() => {
+        // 現在のメッセージを削除
+        $('.username-search-alert').css('display', 'none');
+        const searchName = $('#username-search-input').val();
+        if (searchName === '') {
+            $('.username-search-alert.no-input').css('display', 'block');
+        } else {
+            let found = false;
+            $('#standings-body a[class^=user]').each((_, elem) => {  // :contains()ではダメ
+                if ($(elem).text() === searchName) {
+                    found = true;
+                    // 現在の枠線を削除
+                    $('#standings-body > tr').css('border', 'none');
+                    // 枠線をつける
+                    $(elem).parent().parent().css('border', 'solid 3px #dd289a');
+                    // スクロール
+                    $("html,body").animate({
+                        scrollTop: $(elem).offset().top - $(window).height() / 2
+                    });
+                }
+            });
+            if (!found) {
+                $('.username-search-alert.not-found').css('display', 'block');
+            }
+        }
+    });
+    $('#username-search-input').keypress(pressedKey => {
+        if (pressedKey.which === 13) {
+            // エンターキー
+            $('#username-search-button').click();
+        }
+    });
+
+
     function setItemToSelector(items) {
         var selected = $("#contest-selector").val();
         $("#contest-selector").empty();
