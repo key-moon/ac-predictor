0bd6048051ef9c3c3e8992c526d8de0ff096f415
keymoon <kymn0116@gmail.com>
2019-03-23 11:27:27 +0900

    init nodejs and add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+﻿const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'ac-predictor/main.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        modules: [path.resolve(__dirname, 'src')]
+    },
+    externals: {
+        jquery: 'jQuery'
+    },
+    optimization: {
+        minimize: false
+    }
+};
+
