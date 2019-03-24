830939892dbff6dc0fe2250a649dc49163b681ae
keymoon <kymn0116@gmail.com>
2019-03-24 16:32:11 +0900

    add predictorDB

---
+++ src/libs/database/predictorDB.js
@@ -0,0 +1,10 @@
+﻿import { DataBase } from './database';
+
+export StoreKeys = { aperfs: "APerfs", standings: "Standings" };
+export class PredictorDB extends DataBase {
+    version = 1;
+    name = "PredictorDB";
+    constructor() {
+        super(this.name, this.version);
+    }
+}
