51d85527c4b083a3255e1c1b5cc0b7869e05b040
keymoon <kymn0116@gmail.com>
2019-03-25 17:24:39 +0900

    fix header

--- src/header.js
+++ src/header.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name        ac-predictor
 // @namespace   http://ac-predictor.azurewebsites.net/
-// @version     1.0.2
+// @version     1.0.10
 // @description コンテスト中にAtCoderのパフォーマンスを予測します
 // @author      keymoon
 // @license     MIT
 // @supportURL  https://github.com/key-moon/ac-predictor.user.js/issues
-// @match       https://beta.atcoder.jp/*
-// @exclude     https://beta.atcoder.jp/*/json
+// @match       https://atcoder.jp/*
+// @exclude     https://atcoder.jp/*/json
 // ==/UserScript==
