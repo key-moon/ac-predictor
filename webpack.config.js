12b58442c059e9e44d367a11b9e313b78bf401c4
keymoon <kymn0116@gmail.com>
2019-03-25 15:19:00 +0900

    add html rule and fix path

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,28 @@
 ﻿const path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'ac-predictor/main.js'),
+    entry: path.resolve(__dirname, 'src/main.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [
-                "style-loader", // creates style nodes from JS strings
-                "css-loader", // translates CSS into CommonJS
-                "sass-loader" // compiles Sass to CSS, using Node Sass by default
-            ]
-        }]
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            },
+            {
+                test: /\.(html)$/,
+                use: [
+                    'html-loader'
+                ]
+            }
+        ]
     },
     resolve: {
         modules: [path.resolve(__dirname, 'src')]
