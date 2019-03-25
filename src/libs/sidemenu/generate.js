743c5f1934cb27a28d18d2e8388f13ba9f3c6728
keymoon <kymn0116@gmail.com>
2019-03-25 15:19:41 +0900

    add sidemenu generator

---
+++ src/libs/sidemenu/generate.js
@@ -0,0 +1,28 @@
+﻿import './sidemenu.scss';
+import sidemenuHtml from './sidemenu.html';
+
+export function generateSideMenu() {
+    $('#main-div').append(sidemenuHtml);
+
+    var windowHeight = $(window).height();
+    $('#sidemenu').height(windowHeight);
+
+    //メニュー開閉
+    $('#sidemenu-key').click(function () {
+        $('#sidemenu-key').toggleClass('glyphicon-menu-left');
+        $('#sidemenu-key').toggleClass('glyphicon-menu-right');
+        $('#menu_wrap').toggleClass('sidemenu-active');
+    });
+
+    //画面リサイズ時にheightを読み直し
+    $(window).resize(function () {
+        $('#sidemenu').height($(window).height());
+    });
+
+    //アコーディオンメニュー
+    $('#sidemenu').on('click', '.menu-header', (event) => {
+        $(event.target).parents('.menu-wrapper').find('.menu-box').toggleClass('menu-box-collapse');
+        $(event.target).find('.glyphicon').toggleClass('glyphicon-menu-down');
+        $(event.target).find('.glyphicon').toggleClass('glyphicon-menu-up');
+    });
+}
