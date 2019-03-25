ad037dde2402a23e3e487315b8d90214e9a08fd5
keymoon <kymn0116@gmail.com>
2019-03-25 15:01:59 +0900

    add webpack config for scss

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,16 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
+        }]
+    },
     resolve: {
         modules: [path.resolve(__dirname, 'src')]
     },
