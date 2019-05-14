6acbbe3fbc25440da41bc0dedaa6619827e844a0
keymoon <kymn0116@gmail.com>
2019-05-14 23:44:08 +0900

    [add] colorBounds dictionary

--- src/libs/utils/ratingColor.js
+++ src/libs/utils/ratingColor.js
@@ -1,4 +1,15 @@
-﻿export const colorNames = ["unrated", "gray", "brown", "green", "cyan", "blue", "yellow", "orange", "red"];
+﻿export const colorBounds = {
+    "gray": 0,
+    "brown": 400,
+    "green": 800,
+    "cyan": 1200,
+    "blue": 1600,
+    "yellow": 2000,
+    "orange": 2400,
+    "red": 2800
+};
+
+export const colorNames = ["unrated", "gray", "brown", "green", "cyan", "blue", "yellow", "orange", "red"];
 
 export function getColor(rating) {
     let colorIndex = rating > 0 ? Math.min(Math.floor(rating / 400) + 1, 8) : 0;
