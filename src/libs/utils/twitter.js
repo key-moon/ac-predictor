20edfc78352ca23b40160d7a5ec8d6244309f292
keymoon <kymn0116@gmail.com>
2019-05-14 19:48:43 +0900

    [add] Twitter用のutil関数群

---
+++ src/libs/utils/twitter.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * @param {string} [content]
+ * @param {string} [url]
+ * @return {string}
+ */
+export function GetEmbedTweetLink(content, url){
+   return `https://twitter.com/share?text=${encodeURI(content)}&url=${encodeURI(url)}`
+}
