2a587db7839c8650825d9951e931161a32039d8a
keymoon <kymn0116@gmail.com>
2019-05-14 20:15:29 +0900

    [refactor] estimatorをごそっと綺麗に

--- src/elements/estimator/script.js
+++ src/elements/estimator/script.js
@@ -1,102 +1,86 @@
 import document from "./dom.html"
+import moment from "moment";
 import {SideMenuElement} from "../../libs/sidemenu/element";
 import {HistoryData} from "../../libs/datas/history";
-import {unpositivize_rating, calc_rating} from "../../libs/utils/atcoder_rating";
-
-
+import {CalcPerfModel} from "./state/CalcPerfModel";
+import {CalcRatingModel} from "./state/CalcRatingModel";
+import {getLS, setLS} from "../../atcoder-lib/utils";
+import {GetEmbedTweetLink} from "../../libs/utils/twitter";
 
 export let estimator = new SideMenuElement('estimator','Estimator',/atcoder.jp/, document, afterAppend);
 
-
 async function afterAppend() {
-    const historyData = new HistoryData(userScreenName,() => {});
-    await historyData.update();
-    var estimator_state = parseInt(localStorage.getItem("sidemenu_estimator_state"));
-    $("#estimator-input").val(localStorage.getItem("sidemenu_estimator_value"));
-    updateInputs();
-
-    $("#estimator-input").keyup(updateInputs);
+    const estimatorInputSelector = $("#estimator-input");
+    const estimatorResultSelector = $("#estimator-res");
+    let model = GetModelFromStateCode(getLS("sidemenu_estimator_state"), getLS("sidemenu_estimator_value"), await GetHistory());
 
     $("#estimator-toggle").click(function () {
-        $("#estimator-input").val($("#estimator-res").val());
-        estimator_state = (estimator_state + 1) % 2;
-        updateInputs();
+        model = model.toggle();
+        updateLocalStorage();
+        updateView();
+    });
+    estimatorInputSelector.keyup(() => {
+        updateModel();
+        updateLocalStorage();
+        updateView();
     });
 
-    function updateInputs() {
-        let input = $("#estimator-input").val();
-        if (!isFinite(input)) {
-			displayAlert("数字ではありません");
-            return;
-        }
-
-		let history = historyData.data.filter(x => x.IsRated);
-        history.sort(function (a, b) {
-            if (a.EndTime < b.EndTime) return 1;
-            if (a.EndTime > b.EndTime) return -1;
-            return 0;
-        })
-        history = history.map(x => x.InnerPerformance);
-		input = parseInt(input);
-		let res = -1;
-        if (estimator_state === 0) {
-            // binary search
-			let goal_rating = unpositivize_rating(input);
-			let lo = -10000.0;
-			let hi = 10000.0;
-			for (let i = 0; i < 100; ++i) {
-				let mid = (hi + lo) / 2;
-				let r = calc_rating([mid].concat(history));
-                if (r >= goal_rating) {
-                    hi = mid;
-                } else {
-                    lo = mid;
-                }
-            }
-            res = (hi + lo) / 2;
-            $("#estimator-input-desc").text("目標レーティング");
-            $("#estimator-res-desc").text("必要パフォーマンス");
-        }
-        else {
-            res = calc_rating([input].concat(history));
-            $("#estimator-input-desc").text("パフォーマンス");
-            $("#estimator-res-desc").text("到達レーティング");
-        }
-		res = Math.round(res * 100) / 100;
-        if (!isNaN(res)) $("#estimator-res").val(res);
-        updateLocalStorage();
-        updateTweetBtn();
+    /** modelをinputの値に応じて更新 */
+    function updateModel() {
+        const inputString = estimatorInputSelector.val();
+        if (!inputString || !isFinite(inputString)) return;
+        const inputNumber = parseInt(inputString);
+        model.updateInput(inputNumber);
     }
 
+    /** modelの状態をLSに保存 */
     function updateLocalStorage() {
-        localStorage.setItem("sidemenu_estimator_state", estimator_state);
-        localStorage.setItem("sidemenu_estimator_value", $("#estimator-input").val());
+        setLS("sidemenu_estimator_value", model.inputValue);
+        setLS("sidemenu_estimator_state", model.constructor.name);
     }
 
-    function updateTweetBtn() {
-        var tweetStr =
-            `AtCoderのハンドルネーム: ${userScreenName}%0A
-${estimator_state === 0 ? "目標レーティング" : "パフォーマンス"}: ${$("#estimator-input").val()}%0A
-${estimator_state === 0 ? "必要パフォーマンス" : "到達レーティング"}: ${$("#estimator-res").val()}%0A`;
-        $('#estimator-tweet').attr("href", `https://twitter.com/share?text=${tweetStr}&url=https://greasyfork.org/ja/scripts/369954-ac-predictor`);
-    }
+    /** modelを元にviewを更新 */
+    function updateView() {
+        $("#estimator-input-desc").text(model.inputDesc);
+        $("#estimator-res-desc").text(model.resultDesc);
+        estimatorInputSelector.val(roundVal(model.inputValue, 2));
+        estimatorResultSelector.val(roundVal(model.resultValue, 2));
 
-    function displayAlert(message) {
-		let alertDiv = document.createElement('div');
-		alertDiv.setAttribute("role", "alert");
-		alertDiv.setAttribute("class", "alert alert-warning alert-dismissible");
-		let closeButton = document.createElement('button');
-		closeButton.setAttribute("type", "button");
-		closeButton.setAttribute("class", "close");
-		closeButton.setAttribute("data-dismiss", "alert");
-		closeButton.setAttribute("aria-label", "閉じる");
-		let closeSpan = document.createElement('span');
-		closeSpan.setAttribute("aria-hidden", "true");
-		closeSpan.textContent = "×";
-		closeButton.appendChild(closeSpan);
-		let messageContent = document.createTextNode(message);
-		alertDiv.appendChild(closeButton);
-		alertDiv.appendChild(messageContent);
-		$("#estimator-alert").append(alertDiv);
+        const tweetStr = `AtCoderのハンドルネーム: ${userScreenName}\n${model.inputDesc}: ${model.inputValue}\n${model.resultDesc}: ${model.resultValue}\n`;
+        $('#estimator-tweet').attr("href", GetEmbedTweetLink(tweetStr, "https://greasyfork.org/ja/scripts/369954-ac-predictor"));
+
+        function roundVal(value, digit) {
+            return Math.round(value * (10 ** digit)) / (10 ** digit);
+        }
     }
+}
+
+/**
+ * パフォーマンス履歴を取得し、いい感じで整形して返します
+ * @return {Promise<Number[]>} パフォーマンス履歴を返すpromise
+ */
+async function GetHistory(){
+    return new Promise((resolve) => {
+        new HistoryData(userScreenName,() => {}).update().then((data) => {
+            resolve(data.filter(x => x.IsRated)
+                .sort((a, b) => moment(b.EndTime) - moment(a.EndTime))
+                .map(x => x.Performance));
+        })
+    });
+}
+
+/**
+ * LocalStorageに保存されたステートコードから状態を復元します
+ * @param {string} [state] ステートを示す文字列(型名)
+ * @param {number} [value] 最初に入る値
+ * @param {number[]} [history] パフォーマンス履歴(時間降順)
+ * @return {EstimatorModel} 構築されたモデル
+ */
+function GetModelFromStateCode(state, value, history) {
+    let res ={
+        "CalcRatingState": CalcRatingModel,
+        "CalcPerfState": CalcPerfModel
+    }[state];
+    if (!res) res = CalcPerfModel;
+    return new res(value, history);
 }
