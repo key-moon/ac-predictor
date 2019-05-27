3c765b33358fb28e4f605500cf5c22f1654039d5
keymoon <kymn0116@gmail.com>
2019-05-27 22:39:55 +0900

    [delete] 不要なコメントアウトライブラリ群を削除

--- src/main.js
+++ src/main.js
@@ -1,45 +1,9 @@
-import { initializeID } from './libs/tabID';
-
-initializeID();
-
-/*//モーダル関連
-SideMenu.Modal = {}
-$('body').append('<div class="modal-standing"></div>');
-SideMenu.Modal.SetContent = ((content) => {
-    $('#modal-standing').html = content;
-});
-SideMenu.Modal.Show = (() => {
-    $('#modal-standing').modal('show');
-});
-SideMenu.Modal.Hide = (() => {
-    $('#modal-standing').modal('hide');
-});
-SideMenu.Modal.Toggle = (() => {
-    $('#modal-standing').modal('toggle');
-});
-
-//通知関連
-SideMenu.Notifications = {};
-SideMenu.Notifications.CanSend = false;
-(async () => {
-    if (Notification.permission === 'denied') return;
-    if (Notification.permission === 'default') {
-        await (async () => {
-            var defferd = $.Deferred();
-            Notification.requestPermission((permission) => {
-                SideMenu.Notifications.CanSend = permission === 'granted';
-                defferd.resolve();
-            })();
-            return defferd.promise();
-        });
-        if (!Notification.permission) return;
-    }
-})();*/
-
-
+import {initializeID} from './libs/tabID';
 import {SideMenu} from "./libs/sidemenu/sidemenu";
 import {predictor} from "./elements/predictor/script";
 import {estimator} from "./elements/estimator/script";
+
+initializeID();
 
 let sidemenu = new SideMenu();
 
