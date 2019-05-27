fac48c1eaf19ddd41aba5a1a2a4fe9ed330f657a
keymoon <kymn0116@gmail.com>
2019-05-27 22:40:55 +0900

    [add] Results/jsonを読み込むclassを追加

---
+++ src/libs/datas/results.js
@@ -0,0 +1,12 @@
+﻿import { WebData } from './data';
+
+/**
+ * コンテストの結果データを取得します。
+ */
+export class ResultsData extends WebData {
+    constructor(contestScreenName) {
+        super(
+            `https://atcoder.jp/contests/${contestScreenName}/results/json`
+        );
+    }
+}
