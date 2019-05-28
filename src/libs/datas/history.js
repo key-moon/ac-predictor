89b624b86723ab2e5cc5c87c69d7d7c6a59bd8d4
keymoon <kymn0116@gmail.com>
2019-05-29 03:37:53 +0900

    [add] historyDataはsingletonとして扱えるので、それぞれでやるのは効率が悪い

--- src/libs/datas/history.js
+++ src/libs/datas/history.js
@@ -1,12 +1,35 @@
 ﻿import { WebData } from './data';
+import moment from "moment";
 
-/**
- * ユーザのパフォーマンス履歴を取得し、他のタブと同期的にデータを扱います。
- */
-export class HistoryData extends WebData {
+class HistoryData extends WebData {
     constructor(userScreenName) {
         super(
             `https://atcoder.jp/users/${userScreenName}/history/json`
         );
     }
+}
+
+let historyData = null;
+
+/**
+ * パフォーマンス履歴を取得します。
+ * @return {Promise<{}[]>} パフォーマンス履歴を返すpromise
+ */
+export async function getHistory(){
+    return new Promise((resolve) => {
+        if (historyData) resolve(historyData);
+        new HistoryData(userScreenName).update().then((data) => {
+            resolve(historyData = data);
+        })
+    });
+}
+
+/**
+ * レーテっと
+ * @return {Promise<number[]>}
+ */
+export async function getRatedContestPerformanceHistory() {
+    return  getHistory().then((data) => data.filter(x => x.IsRated)
+        .sort((a, b) => moment(b.EndTime) - moment(a.EndTime))
+        .map(x => x.Performance));
 }
