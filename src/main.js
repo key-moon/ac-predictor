ac28365724a945b0ff248eea13a0d2c59e7071fb
keymoon <kymn0116@gmail.com>
2019-05-28 16:29:08 +0900

    [change] headerをmainに置くように

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+// ==UserScript==
+// @name        ac-predictor
+// @namespace   http://ac-predictor.azurewebsites.net/
+// @version     1.1.1
+// @description コンテスト中にAtCoderのパフォーマンスを予測します
+// @author      keymoon
+// @license     MIT
+// @supportURL  https://github.com/key-moon/ac-predictor.user.js/issues
+// @match       https://atcoder.jp/*
+// @exclude     https://atcoder.jp/*/json
+// ==/UserScript==
+
 import {initializeID} from './libs/tabID';
 import {SideMenu} from "./libs/sidemenu/sidemenu";
 import {predictor} from "./elements/predictor/script";
