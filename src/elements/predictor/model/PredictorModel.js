4f19aee2d46b9c76075ca19f7fbeb50382018d08
keymoon <kymn0116@gmail.com>
2019-06-03 01:08:47 +0900

    [add] jsdoc/setEnable

--- src/elements/predictor/model/PredictorModel.js
+++ src/elements/predictor/model/PredictorModel.js
@@ -5,16 +5,32 @@ export class PredictorModel{
      * @param {PredictorModel} [model]
      */
     constructor(model){
+        this.enabled = model.enabled;
         this.contest = model.contest;
         this.history = model.history;
         this.updateInformation(model.information);
-        this.updateInformation(model.rankValue, model.perfValue, model.rateValue);
+        this.updateData(model.rankValue, model.perfValue, model.rateValue);
     }
 
+    /**
+     * @param {boolean} state
+     */
+    setEnable(state){
+        this.enabled = state;
+    }
+
+    /**
+     * @param {string} information
+     */
     updateInformation(information){
         this.information = information;
     }
 
+    /**
+     * @param {number} rankValue
+     * @param {number} perfValue
+     * @param {number} rateValue
+     */
     updateData(rankValue, perfValue, rateValue){
         this.rankValue = rankValue;
         this.perfValue = perfValue;
