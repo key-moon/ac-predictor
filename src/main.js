ec772aaf9d2099e6c9886e846c0c1327974eff37
keymoon <kymn0116@gmail.com>
2019-06-10 01:54:25 +0900

    version up to 1.2.4

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        ac-predictor
 // @namespace   http://ac-predictor.azurewebsites.net/
-// @version     1.2.3
+// @version     1.2.4
 // @description コンテスト中にAtCoderのパフォーマンスを予測します
 // @author      keymoon
 // @license     MIT
