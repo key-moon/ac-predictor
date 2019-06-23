d9639c67c93e32c111f4f38e0d498bc327540ada
keymoon <kymn0116@gmail.com>
2019-06-24 00:17:23 +0900

    [install] eslint用のprettierプラグイン

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": "eslint:recommended",
+    "extends": ["eslint:recommended", "plugin:prettier/recommended"],
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
