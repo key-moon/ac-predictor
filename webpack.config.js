8808dcab52eaa5d54e7c267518429e2b778f5534
keymoon <kymn0116@gmail.com>
2019-06-24 00:31:25 +0900

    [fix] webpack.config.jsにprettierを適用

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-﻿const path = require('path');
+﻿const path = require("path");
 
 module.exports = {
     mode: "production",
-    entry: path.resolve(__dirname, 'src/main.js'),
+    entry: path.resolve(__dirname, "src/main.js"),
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
     },
     module: {
         rules: [
@@ -19,27 +19,25 @@ module.exports = {
             },
             {
                 test: /\.(html)$/,
-                use: [
-                    'html-loader'
-                ]
+                use: ["html-loader"]
             }
         ]
     },
     resolve: {
-        modules: [path.resolve(__dirname, 'src')]
+        modules: [path.resolve(__dirname, "src")]
     },
     externals: {
-        jquery: 'jQuery',
-        moment: 'moment',
-        'atcoder-sidemenu': 'sidemenu',
-        'atcoder-userscript-libs': 'usLibs',
-        'atcoder-userscript-libs/src/libs/data': 'usLibs.data',
-        'atcoder-userscript-libs/src/libs/rating': 'usLibs.rating',
-        'atcoder-userscript-libs/src/libs/global': 'usLibs.global',
-        'atcoder-userscript-libs/src/libs/contestInformation': 'usLibs.contestInformation'
+        jquery: "jQuery",
+        moment: "moment",
+        "atcoder-sidemenu": "sidemenu",
+        "atcoder-userscript-libs": "usLibs",
+        "atcoder-userscript-libs/src/libs/data": "usLibs.data",
+        "atcoder-userscript-libs/src/libs/rating": "usLibs.rating",
+        "atcoder-userscript-libs/src/libs/global": "usLibs.global",
+        "atcoder-userscript-libs/src/libs/contestInformation":
+            "usLibs.contestInformation"
     },
     optimization: {
         minimize: false
     }
 };
-
