c67e33632ef41dffb38ae7a1f8287213b76a728d
keymoon <kymn0116@gmail.com>
2020-05-14 20:16:19 +0900

    add property for serialization

--- AtCoder/Model/CompetitionResult.cs
+++ AtCoder/Model/CompetitionResult.cs
@@ -1,18 +1,42 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace AtCoder
 {
+    [JsonObject]
     public partial class CompetitionResult
     {
+        [JsonProperty]
         public bool IsRated { get; private set; }
+        [JsonProperty]
         public int Place { get; private set; }
+        [JsonProperty]
         public int OldRating { get; private set; }
+        [JsonProperty]
         public int NewRating { get; private set; }
-        public int Perfomance { get; private set; }
-        public int InnerPerfomance { get; private set; }
-        public string ContestScreenName { get; private set; }
+        [JsonProperty]
+        public int Performance { get; private set; }
+        [JsonProperty]
+        public int InnerPerformance { get; private set; }
+        [JsonProperty]
         public string ContestName { get; private set; }
+        [JsonProperty]
         public string ContestNameEn { get; private set; }
+        [JsonProperty]
+        public string ContestScreenName { get; private set; }
+        [JsonProperty]
         public DateTime EndTime { get; private set; }
+        [JsonProperty]
+        public string UserName { get; private set; }
+        [JsonProperty]
+        public string UserScreenName { get; private set; }
+        [JsonProperty]
+        public string Country { get; private set; }
+        [JsonProperty]
+        public string Affiliation { get; private set; }
+        [JsonProperty]
+        public string Rating { get; private set; }
+        [JsonProperty]
+        public string Competitions { get; private set; }
     }
 }
