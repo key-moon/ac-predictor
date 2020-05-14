49939b1234ee6c2e4f0ffa3ec1e71ef445e43980
keymoon <kymn0116@gmail.com>
2020-05-15 00:42:07 +0900

    add contestInfo

---
+++ AtCoder/Model/ContestInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtCoder
+{
+    public partial class ContestInfo
+    {
+        public DateTime StartTime { get; set; }
+        public string ContestName { get; set; }
+        public string ContestScreenName { get; set; }
+        public TimeSpan Duration { get; set; }
+        public Range RatedRange { get; set; }
+    }
+
+    public partial struct Range
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+    }
+}
