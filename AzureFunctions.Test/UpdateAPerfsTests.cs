292de9d015f75be158bd7746c957dedc9ae424af
keymoon <kymn0116@gmail.com>
2020-06-01 16:26:38 +0900

    add updateaperfs

---
+++ AzureFunctions.Test/UpdateAPerfsTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using AzureFunctions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System.Threading.Tasks;
+
+namespace AzureFunctions.Tests
+{
+    [TestFixture()]
+    public class UpdateAPerfsTests
+    {
+        [Test()]
+        public async Task RunTest()
+        {
+            await UpdateAPerfs.Run("agc002", new Mock<ILogger>().Object);
+        }
+    }
+}
