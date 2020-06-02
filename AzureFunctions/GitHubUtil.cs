d5f6e579646e429fdfa2c038cff7cba7feb9d7fa
keymoon <kymn0116@gmail.com>
2020-06-02 12:42:32 +0900

    add GitHubUtil

---
+++ AzureFunctions/GitHubUtil.cs
@@ -0,0 +1,21 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureFunctions
+{
+    static class GitHubUtil
+    {
+        public const string Owner = "key-moon";
+        public const string Repo = "ac-predictor-data";
+        public static readonly string Token = Secrets.GetSecret("GitHubToken");
+        public static readonly Committer Comitter = new Committer("ac-predictor", "kymn0116+predictor@gmail.com", DateTimeOffset.Now);
+        public static GitHubClient GetClient()
+        {
+            var client = new GitHubClient(new ProductHeaderValue("ac-predictor-function"));
+            client.Credentials = new Credentials(Token);
+            return client;
+        }
+    }
+}
