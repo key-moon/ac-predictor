a9706195755bc329533d765100f794348707f19f
keymoon <kymn0116@gmail.com>
2020-06-02 12:39:11 +0900

    add secret

---
+++ AzureFunctions/Secrets.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureFunctions
+{
+    static class Secrets
+    {
+        static IConfiguration Configuration { get; set; } = new ConfigurationBuilder()
+                .AddUserSecrets<Class>().Build();
+        public static string GetSecret(string key)
+        {
+            var res = Environment.GetEnvironmentVariable(key, EnvironmentVariableTarget.Process);
+            if (res is null) res = Configuration[key];
+            return res;
+        }
+    }
+    class Class { }
+}
