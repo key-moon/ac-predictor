f57d7cd7706cf3f3956b9e62f17b49bfa9dc2f84
keymoon <kymn0116@gmail.com>
2020-06-02 12:43:20 +0900

    add util class for Rating system

---
+++ AtCoder/Util/APerf.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtCoder.Util
+{
+    public partial class Rating
+    {
+        public static double? CalcAPerf(IEnumerable<int> perfs)
+        {
+            if (perfs is null) return null;
+            double denominator = 0;
+            double numerator = 0;
+            foreach (var perf in perfs)
+            {
+                denominator *= 0.9;
+                denominator += perf;
+                numerator *= 0.9;
+                numerator += 1;
+            }
+            if (numerator == 0) return null;
+            return denominator / numerator;
+        }
+    }
+}
