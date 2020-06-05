5ab765c22c235bc3f97a99970ee66faa4b419509
keymoon <kymn0116@gmail.com>
2020-06-05 19:51:53 +0900

    remove old project

--- ac-predictor/Scripts/_references.js
+++
@@ -1,3 +0,0 @@
-﻿/// <reference path="modernizr-2.8.3.js" />
-/// <reference path="jquery-3.3.1.js" />
-/// <reference path="bootstrap.js" />
--- ac-predictor/Scripts/base.js
+++
@@ -1,8 +0,0 @@
-﻿const colors = ["unrated", "gray", "brown", "green", "cyan", "blue", "yellow", "orange", "red"]
-function getColor(rating) {
-	var colorIndex = 0
-	if (rating > 0) {
-		colorIndex = Math.min(Math.floor(rating / 400) + 1, 8)
-	}
-	return colors[colorIndex]
-}
