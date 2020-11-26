3030a04be012c0f42e47e43ed15ec08dd9c0e568
keymoon <kymn0116@gmail.com>
2020-11-26 22:15:45 +0900

    add jest

---
+++ ac-predictor-extension/jest.config.js
@@ -0,0 +1,11 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+export default {
+    clearMocks: true,
+    coverageDirectory: "coverage",
+    coveragePathIgnorePatterns: ["/node_modules/"],
+    coverageProvider: "babel"
+};
