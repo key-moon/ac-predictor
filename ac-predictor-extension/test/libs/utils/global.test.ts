f558525b1a96703e1f4df349111750237ee0198e
keymoon <kymn0116@gmail.com>
2020-11-27 13:39:56 +0900

    add test for global.ts

---
+++ ac-predictor-extension/test/libs/utils/global.test.ts
@@ -0,0 +1,17 @@
+import * as fs from "fs";
+
+const html = fs.readFileSync(__dirname + "/global.html");
+
+const dom = new DOMParser().parseFromString(html.toString(),"text/html");
+Object.defineProperty(window, "document", { writable: true, value: dom });
+
+
+import { globalVals } from "../../../src/libs/utils/global";
+
+test("parse check", () => {
+    expect(globalVals["str"]).toBe("str");
+    expect(globalVals["obj"]).toStrictEqual({ a: "a", b: "b" });
+    expect(globalVals["num"]).toBe(1);
+    expect(globalVals["arr"]).toStrictEqual([1, 2, 3]);
+    expect(globalVals["body"]).toBeUndefined();
+});
