1d3290fa4e9bb72e821807e30e07168dab6b509b
keymoon <kymn0116@gmail.com>
2021-01-17 06:20:39 +0900

    avoid to use eval

--- ac-predictor-extension/src/libs/utils/global.ts
+++ ac-predictor-extension/src/libs/utils/global.ts
@@ -3,7 +3,25 @@ function getGlobalVals(): {} {
     const res = {};
     script.match(/var [^ ]+ = .+$/gm).forEach(statement => {
         const match = /var ([^ ]+) = (.+)$/m.exec(statement);
-        res[match[1]] = eval(match[2]);
+        function safeEval(val: string): unknown {
+            function trim(val: string): string {
+                while (val.endsWith(";") || val.endsWith(" ")) val = val.substr(0, val.length - 1);
+                while (val.startsWith(" ")) val = val.substr(1, val.length - 1);
+                return val;
+            }
+            function isStringToken(val: string): boolean {
+                return 1 < val.length && val.startsWith('"') && val.endsWith('"');
+            }
+            function evalStringToken(val: string): string {
+                if (!isStringToken(val)) throw new Error();
+                return val.substr(1, val.length - 2); // TODO: parse escape
+            }
+            val = trim(val);
+            if (isStringToken(val)) return evalStringToken(val);
+            if (val.startsWith("moment(")) return new Date(evalStringToken(trim(val.substr(7, val.length - (7 + 1)))));
+            return val;
+        }
+        res[match[1]] = safeEval(match[2]);
     });
     return res;
 }
@@ -16,5 +34,5 @@ export const csrfToken: string = globalVals["csrfToken"];
 export const contestScreenName: string = globalVals["contestScreenName"];
 export const countDownText: string = globalVals["countDownText"];
 export const remainingText: string = globalVals["remainingText"];
-export const startTime: Date = globalVals["startTime"]?.toDate();
-export const endTime: Date = globalVals["endTime"]?.toDate();
+export const startTime: Date = globalVals["startTime"];
+export const endTime: Date = globalVals["endTime"];
