8943119834960a67343e8f7f0adc5850b72a6545
keymoon <kymn0116@gmail.com>
2021-01-20 06:24:54 +0900

    add standingsRowModifier

---
+++ ac-predictor-extension/src/elements/predictor/view/standingsRowModifier/perfAndRateChangeAppender.ts
@@ -0,0 +1,70 @@
+import { Results } from "../../../../libs/contest/results/results";
+import { getColor, positivizeRating } from "../../../../libs/utils/rating";
+import { StandingsRowModifier } from "./standingsRowModifier";
+
+export class PerfAndRateChangeAppender extends StandingsRowModifier {
+    results: Results;
+    isRated: boolean;
+
+    constructor(results: Results, isRated: boolean) {
+        super();
+        this.results = results;
+        this.isRated = isRated;
+    }
+
+    modifyContent(content: HTMLTableRowElement): void {
+        this.removeOldElem(content);
+        if (content.firstElementChild.textContent === "-") {
+            const longCell = content.getElementsByClassName("standings-result")[0];
+            longCell.setAttribute("colspan", (parseInt(longCell.getAttribute("colspan")) + 2).toString());
+            return;
+        }
+        const userScreenName = content.querySelector(".standings-username .username span").textContent;
+        const result = this.results?.getUserResult(userScreenName);
+        const performance = Math.round(positivizeRating(result.Performance));
+
+        const perfElem = result.IsSubmitted ? this.getRatingSpan(performance) : "-";
+        const ratingElem =
+            result?.IsRated && this?.isRated
+                ? this.getChangedRatingElem(result.OldRating, result.NewRating)
+                : this.getUnratedElem(result.OldRating);
+
+        content.insertAdjacentHTML("beforeend", `<td class="standings-result standings-perf">${perfElem}</td>`);
+        content.insertAdjacentHTML("beforeend", `<td class="standings-result standings-rate">${ratingElem}</td>`);
+    }
+    getChangedRatingElem(oldRate: number, newRate: number): string {
+        const oldRateSpan = this.getRatingSpan(oldRate);
+        const newRateSpan = this.getRatingSpan(newRate);
+        const diff = this.toSignedString(newRate - oldRate);
+        return `<span class="bold">${oldRateSpan}</span> → <span class="bold">${newRateSpan}</span> <span class="grey">(${diff})</span>`;
+    }
+    toSignedString(n: number): string {
+        return `${n >= 0 ? "+" : ""}${n}`;
+    }
+    getUnratedElem(rate: number): string {
+        return `<span class="bold">${this.getRatingSpan(rate)}</span> <span class="grey">(unrated)</span>`;
+    }
+    getRatingSpan(rate: number): string {
+        return `<span class="user-${getColor(rate)}">${rate}</span>`;
+    }
+
+    modifyFooter(footer: HTMLTableRowElement): void {
+        this.removeOldElem(footer);
+        footer.insertAdjacentHTML(
+            "beforeend",
+            '<td class="standings-result standings-perf standings-rate" colspan="2">-</td>'
+        );
+    }
+
+    modifyHeader(header: HTMLTableRowElement): void {
+        this.removeOldElem(header);
+        header.insertAdjacentHTML(
+            "beforeend",
+            '<th class="standings-result-th" style="width:84px;min-width:84px;">perf</th><th class="standings-result-th" style="width:168px;min-width:168px;">レート変化</th>'
+        );
+    }
+
+    removeOldElem(row: HTMLTableRowElement): void {
+        row.querySelectorAll(".standings-perf, .standings-rate").forEach((elem) => elem.remove());
+    }
+}
---
+++ ac-predictor-extension/src/elements/predictor/view/standingsRowModifier/standingsRowModifier.ts
@@ -0,0 +1,17 @@
+export abstract class StandingsRowModifier {
+    isHeader(row: HTMLTableRowElement): boolean {
+        return row.parentElement.tagName == "thead";
+    }
+    isFooter(row: HTMLTableRowElement): boolean {
+        return row.firstElementChild.hasAttribute("colspan") && row.firstElementChild.getAttribute("colspan") == "3";
+    }
+    modifyRow(row: HTMLTableRowElement): void {
+        if (this.isHeader(row)) this.modifyHeader(row);
+        else if (this.isFooter(row)) this.modifyFooter(row);
+        else this.modifyContent(row);
+    }
+
+    abstract modifyHeader(header: HTMLTableRowElement): void;
+    abstract modifyContent(content: HTMLTableRowElement): void;
+    abstract modifyFooter(footer: HTMLTableRowElement): void;
+}
