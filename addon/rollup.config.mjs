bfb38dd9829fdbd32edc4163a418d21ce8cd993e
key-moon <kymn0116@gmail.com>
2023-07-02 02:29:30 +0900

    init addon dir

---
+++ addon/rollup.config.mjs
@@ -0,0 +1,38 @@
+import typescript from "rollup-plugin-typescript";
+import html from "rollup-plugin-html";
+import scss from 'rollup-plugin-scss';
+import packageJson from "./package.json" assert { type: "json" };
+
+const namespace = "http://ac-predictor.azurewebsites.net/";
+
+const userScriptBanner = `
+// ==UserScript==
+// @name         ${packageJson.name}
+// @namespace    ${namespace}
+// @version      ${packageJson.version}
+// @description  ${packageJson.description}
+// @author       ${packageJson.author}
+// @license      ${packageJson.license}
+// @match        https://atcoder.jp/*
+// @exclude      https://atcoder.jp/*/json
+// @grant        none
+// ==/UserScript==`.trim();
+
+export default [
+  {
+    input: "src/main.ts",
+    output: {
+      banner: userScriptBanner,
+      file: "dist/userscript/dist.js"
+    },
+    plugins: [
+      html({
+        include: "**/*.html"
+      }),
+      scss({
+        output: false
+      }),
+      typescript()
+    ]
+  }
+];
