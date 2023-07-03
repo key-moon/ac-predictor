86a0fdd699746391af92aa6da9bc532c642d78f6
key-moon <kymn0116@gmail.com>
2023-07-03 20:03:59 +0900

    add tests for malicious cases

--- addon/src/service/performanceprovider/elo.test.ts
+++ addon/src/service/performanceprovider/elo.test.ts
@@ -14,6 +14,11 @@ test("getPerformance should return proper values", () => {
   expect(provider.getPerformance("user5")).toBe(-1010);
 })
 
+test("getPerformance should raise error when unknown key specified", () => {
+  expect(() => provider.getPerformance("toString")).toThrowError("User toString not found");
+  expect(() => provider.getPerformance("__proto__")).toThrowError("User __proto__ not found");
+});
+
 test("getPerformances should return proper value", () => {
   expect(provider.getPerformances()).toStrictEqual({ "user1": 2019, "user2": 1230, "user3": 415, "user4": 415, "user5": -1010 });
 })
--- addon/src/service/performanceprovider/fixed.test.ts
+++ addon/src/service/performanceprovider/fixed.test.ts
@@ -11,8 +11,13 @@ test("getPerformance should return proper values", () => {
   expect(provider.getPerformance("user2")).toBe(100);
   expect(provider.getPerformance("user3")).toBe(-0.01);
   expect(provider.getPerformance("constructor")).toBe(1);
-})
+});
+
+test("getPerformance should raise error when unknown key specified", () => {
+  expect(() => provider.getPerformance("toString")).toThrowError("User toString not found");
+  expect(() => provider.getPerformance("__proto__")).toThrowError("User __proto__ not found");
+});
 
 test("getPerformances should return proper value", () => {
   expect(provider.getPerformances()).toStrictEqual({ "user1": 0, "user2": 100, "user3": -0.01, "constructor": 1 });
-})
+});
