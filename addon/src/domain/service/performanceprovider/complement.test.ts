affe5e168387c8f18e65f8a61343c3588b247bb7
key-moon <kymn0116@gmail.com>
2023-07-05 14:18:38 +0900

    add complementperformanceprovider

---
+++ addon/src/domain/service/performanceprovider/complement.test.ts
@@ -0,0 +1,43 @@
+import ComplementPerformanceProvider from "./complement";
+import PerformanceProvider from "./performancepredictor";
+
+let provider: ComplementPerformanceProvider;
+let basePerformanceProviderMock: PerformanceProvider
+
+beforeEach(() => {
+  basePerformanceProviderMock = {
+    availableFor: jest.fn((val) => val == "user3" || val == "user5"),
+    getPerformance: jest.fn((val) => { if (val == "user3") return 2; if (val == "user5") return 1; throw new Error(); }),
+    getPerformances: jest.fn()
+  };
+  provider = new ComplementPerformanceProvider({ "user1": 1, "user2": 2, "user3": 2, "user4": 4, "user5": 5, "user6": 6 }, basePerformanceProviderMock);
+});
+
+test("availableFor should return proper values", () => {
+  expect(provider.availableFor("user1")).toBe(true);
+  expect(provider.availableFor("user2")).toBe(true);
+  expect(provider.availableFor("user3")).toBe(true);
+  expect(provider.availableFor("user4")).toBe(true);
+  expect(provider.availableFor("user5")).toBe(true);
+  expect(provider.availableFor("user6")).toBe(true);
+  expect(provider.availableFor("toString")).toBe(false);
+  expect(provider.availableFor("__proto__")).toBe(false);
+});
+
+test("getPerformance should return proper values", () => {
+  expect(provider.getPerformance("user1")).toBe(2);
+  expect(provider.getPerformance("user2")).toBe(2);
+  expect(provider.getPerformance("user3")).toBe(2);
+  expect(provider.getPerformance("user4")).toBe(1);
+  expect(provider.getPerformance("user5")).toBe(1);
+  expect(provider.getPerformance("user6")).toBe(-Infinity);
+})
+
+test("getPerformance should raise error when unknown key specified", () => {
+  expect(() => provider.getPerformance("toString")).toThrowError("User toString not found");
+  expect(() => provider.getPerformance("__proto__")).toThrowError("User __proto__ not found");
+});
+
+test("getPerformances should return proper value", () => {
+  expect(provider.getPerformances()).toStrictEqual({ "user1": 2, "user2": 2, "user3": 2, "user4": 1, "user5": 1, "user6": -Infinity });
+});
---
+++ addon/src/domain/service/performanceprovider/complement.ts
@@ -0,0 +1,83 @@
+import hasOwnProperty from "../../../util/hasOwnProperty";
+import PerformanceProvider from "./performancepredictor";
+
+type Ranks = { [userScreenName: string]: number }
+type RankToUsers = { [rank: number]: string[] }
+
+function getRankToUsers(ranks: Ranks) {
+  const rankToUsers: RankToUsers = {};
+  for (const userScreenName in ranks) {
+    const rank = ranks[userScreenName];
+    if (!rankToUsers[rank]) rankToUsers[rank] = [];
+    rankToUsers[rank].push(userScreenName);
+  }
+  return rankToUsers;
+}
+function getMaxRank(ranks: Ranks) {
+  return Math.max(...Object.values(ranks));
+}
+
+// TODO: 命名
+class ComplementPerformanceProvider implements PerformanceProvider {
+  private ranks: Ranks;
+  private maxRank: number;
+  private rankToUsers: RankToUsers;
+  private baseProvider: PerformanceProvider;
+  constructor(ranks: Ranks, baseProvider: PerformanceProvider) {
+    this.ranks = ranks;
+    this.maxRank = getMaxRank(ranks);
+    this.rankToUsers = getRankToUsers(ranks);
+    this.baseProvider = baseProvider;
+  }
+
+  availableFor(userScreenName: string): boolean {
+    return hasOwnProperty(this.ranks, userScreenName);
+  }
+
+  getPerformance(userScreenName: string): number {
+    if (!this.availableFor(userScreenName)) {
+      throw new Error(`User ${userScreenName} not found`);
+    }
+
+    if (this.getPerformancesCache) return this.getPerformancesCache[userScreenName];
+    let rank = this.ranks[userScreenName];
+    while (rank <= this.maxRank) {
+      const perf = this.getPerformanceIfAvailable(rank);
+      if (perf !== undefined) return perf;
+      rank++;
+    }
+    return -Infinity;
+  }
+  
+  private getPerformancesCache?: { [userScreenName: string]: number; };
+  getPerformances(): { [userScreenName: string]: number; } {
+    if (this.getPerformancesCache) return this.getPerformancesCache;
+
+    let currentPerformance = -Infinity;
+    const res: { [userScreenName: string]: number; } = {};
+    for (let rank = this.maxRank; rank >= 0; rank--) {
+      const users = this.rankToUsers[rank];
+      if (!users) continue;
+      const perf = this.getPerformanceIfAvailable(rank);
+      if (perf !== undefined) currentPerformance = perf;
+      for (const userScreenName of users) {
+        res[userScreenName] = currentPerformance;
+      }
+    }
+    return this.getPerformancesCache = res;
+  }
+
+  private cacheForRank: { [rank: number]: number; } = {};
+  private getPerformanceIfAvailable(rank: number) {
+    if (!this.rankToUsers[rank]) return;
+    if (this.cacheForRank[rank]) return this.cacheForRank[rank];
+
+    for (const userScreenName of this.rankToUsers[rank]) {
+      if (!this.baseProvider.availableFor(userScreenName)) continue;
+      return this.cacheForRank[rank] = this.baseProvider.getPerformance(userScreenName);
+    }
+    return;
+  }
+}
+
+export default ComplementPerformanceProvider
