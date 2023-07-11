7a701e49042d757d2ab06b322cab600344594047
key-moon <kymn0116@gmail.com>
2023-07-11 17:32:53 +0900

    fix setup.py

--- crawler/setup.py
+++ crawler/setup.py
@@ -3,7 +3,11 @@ from setuptools import setup
 setup(
     name="ac-predictor-crawler",
     version="0.0.1",
-    install_requires=[],
+    install_requires=[
+        "beautifulsoup4==4.12.2",
+        "tqdm==4.65.0",
+        "requests==2.31.0"
+    ],
     extras_require={
     },
     entry_points={
