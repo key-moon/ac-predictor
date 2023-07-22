d36597b543f3a56ae94fa09caffe1b49d61be969
key-moon <kymn0116@gmail.com>
2023-07-22 14:11:52 +0900

    add method for convertion

--- addon/src/data/results.ts
+++ addon/src/data/results.ts
@@ -21,9 +21,17 @@ type Result = {
 
 const STANDINGS_CACHE_DURATION = 10 * 1000;
 
+export function convertToResultObject(results: Result[]): { [userScreenName: string]: number } {
+  const res: { [userScreenName: string]: number } = {};
+  for (const result of results) {
+    res[result.UserScreenName] = result.Performance;
+  }
+  return res;
+}
+
 const cacheExpires = new Map<string, Date>();
 const cacheData = new Map<string, Result[]>();
-export default async function getResults(contestScreenName: string): Promise<Result[]> {
+export async function getResults(contestScreenName: string): Promise<Result[]> {
   const now = new Date();
   if (!cacheExpires.has(contestScreenName) || cacheExpires.get(contestScreenName)! < now) {
     const result = await fetch(`https://atcoder.jp/contests/${contestScreenName}/results/json`);
