84069889c3cdc6b99a2a68248c3b7d13120811d7
key-moon <kymn0116@gmail.com>
2023-07-29 22:00:08 +0900

    fix virtual participation check

--- addon/src/parse/duringVirtualParticipation.ts
+++ addon/src/parse/duringVirtualParticipation.ts
@@ -4,6 +4,7 @@ export default function duringVirtualParticipation() {
   if (!isVirtualStandingsPage()) {
     throw new Error("not available in this page");
   }
-  if (document.getElementById("virtual-timer")) return true;
+  const timerText = document.getElementById("virtual-timer")?.textContent ?? "";
+  if (timerText && !timerText.includes("終了") && !timerText.includes("over")) return true;
   else return false;
 }
