42d7da7cc8d04c2482cbd1d273f0f79bda128396
key-moon <kymn0116@gmail.com>
2023-07-30 17:00:54 +0900

    update crawler

--- crawler/healthcheck.sh
+++ crawler/healthcheck.sh
@@ -4,5 +4,9 @@ if [ -z "$REPOSITORY_PATH" ] || [ -z "$HEALTHCHECK_UUID" ]; then
   exit 1
 fi
 
-ac-predictor-crawler-runner > /tmp/crawler.log 2>&1
-curl -fsS -m 10 --retry 5 --data-binary @/tmp/crawler.log https://hc-ping.com/$HEALTHCHECK_UUID/$?
+logfile_name="/tmp/crawler-$(date '+%Y%m%d-%H%M%S').log"
+
+ac-predictor-crawler-runner > $logfile_name 2>&1
+curl -fsS -m 10 --retry 5 --data-binary "@$logfile_name" https://hc-ping.com/$HEALTHCHECK_UUID/$?
+
+rm $logfile_name
