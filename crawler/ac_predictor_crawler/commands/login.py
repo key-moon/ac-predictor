98d608f14638eae8f05bb633a664a928911e0d69
key-moon <kymn0116@gmail.com>
2025-03-16 17:05:14 +0900

    use raw session instead of username / password

--- crawler/ac_predictor_crawler/commands/login.py
+++ crawler/ac_predictor_crawler/commands/login.py
@@ -9,21 +9,26 @@ from ac_predictor_crawler.requests import get_atcodersession
 
 def _initializer(parser: ArgumentParser):
   parser.add_argument("user_screen_name", action="store", nargs="?")
+  parser.add_argument("--session", action="store", help="Directly provide session for login")
 
 def _handler(res: Namespace):
-  if res.user_screen_name is None:
-    user_screen_name = input("user_screen_name: ")
+  if res.session:
+    session = res.session
+    logger.info("Using provided session")
   else:
-    user_screen_name = res.user_screen_name
-  print("password:", end="")
-  password = getpass.getpass()
+    if res.user_screen_name is None:
+      user_screen_name = input("user_screen_name: ")
+    else:
+      user_screen_name = res.user_screen_name
+    print("password:", end="")
+    password = getpass.getpass()
 
-  session = get_atcodersession()
-  if session.login(user_screen_name, password):
-    logger.info("login succeeded")
-  else:
-    logger.error("login failed")
-    exit(1)
+    session = get_atcodersession()
+    if session.login(user_screen_name, password):
+      logger.info("login succeeded")
+    else:
+      logger.error("login failed")
+      exit(1)
 
 login_command = SubCommand(
   "login",
