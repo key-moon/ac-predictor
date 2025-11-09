a3660e31fac283a0c85d6ea5eea617826f9ea4b4
key-moon <kymn0116@gmail.com>
2025-11-09 18:51:59 +0900

    implement default changes

--- addon/src/domain/contestdetails.ts
+++ addon/src/domain/contestdetails.ts
@@ -22,28 +22,44 @@ export default class ContestDetails {
   }
   
   public get defaultAPerf(): number {
-    if (this.contestType == "heuristic") return 1000;
-
-    if (!this.ratedrange.hasValue()) {
-      throw new Error("unrated contest");
+    if (this.contestType == "heuristic") {
+      return 1000;
     }
+    else { // algo
+      if (!this.ratedrange.hasValue()) {
+        throw new Error("unrated contest");
+      }
+      if (!this.ratedrange.contains(0)) {
+        return 0; // value is not relevant as it is never used
+      }
 
-    // value is not relevant as it is never used
-    if (!this.ratedrange.contains(0)) return 800;
+      // ref: https://atcoder.jp/posts/1591
+      const DEFAULT_CHANGED_TO_1200 = new Date("2025-11-01");
+      if (DEFAULT_CHANGED_TO_1200 < this.startTime) {
+        return 1200;
+      }
+      // TODO: find default value; it should have changed when the new ABC was introduced
+      // const FIRST_DEFAULT_CHANGES = new Date("2019-05-25");
+      // if (FIRST_DEFAULT_CHANGES < this.startTime) { ... }
 
-    if (this.ratedrange.end == 1199) return 800;
-    if (this.ratedrange.end == 1999) return 800;
-
-    const DEFAULT_CHANGED_AT = new Date("2019-05-25"); // maybe wrong
-    if (this.ratedrange.end == 2799) {
-      if (this.startTime < DEFAULT_CHANGED_AT) return 1600;
-      else return 1000;
+      // ref: AtCoder Rating System ver. 1.00
+      // APerf of newcomers are set to Center, where Center = 1200 for AGC,
+      // Center = 1000 for ARC and Center = 800 for ABC
+      // old ABC
+      if (this.ratedrange.end == 1199) {
+        return 800;
+      }
+      // new ABC
+      if (this.ratedrange.end == 1999) {
+        return 800;
+      }
+      // ARC
+      if (this.ratedrange.end == 2799) {
+        return 1000;
+      }
+      // AGC
+      return 1200;
     }
-    if (4000 <= this.ratedrange.end) {
-      if (this.startTime < DEFAULT_CHANGED_AT) return 1600;
-      else return 1200;
-    }
-
     throw new Error("unknown contest type");
   }
 
